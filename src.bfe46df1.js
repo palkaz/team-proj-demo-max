cf83d8b7c9fe6a1d98bb2aa194a8866bd14ae04a
palkaz <97454178+palkaz@users.noreply.github.com>
2022-05-25 17:33:50 +0000

    Deploying to gh-pages from @ palkaz/team-proj-demo-max@c50f644d7c8bc9fef08386b4a94374e1e9ede9b4 🚀

--- src.bfe46df1.js
+++ src.bfe46df1.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-proj-demo-max/src.f02fcf23.js.map
+//# sourceMappingURL=/team-proj-demo-max/src.bfe46df1.js.map
